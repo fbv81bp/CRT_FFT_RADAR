81c353751bb55d87a2c3092b7f480c567bdb48b1
Balazs Valer Fekete <48908633+fbv81bp@users.noreply.github.com>
2022-08-21 11:03:05 +0200

    Update crt.m

--- crt.m
+++ crt.m
@@ -5,12 +5,12 @@ a = (mod(position-phases(1),wavelengths(1)) <= imprecision);
 b = (mod(position-phases(2),wavelengths(2)) <= imprecision);
 c = (mod(position-phases(3),wavelengths(3)) <= imprecision);
 d = (mod(position-phases(4),wavelengths(4)) <= imprecision);
-e = (mod(position-phases(5),wavelengths(5)) <= imprecision);
+e_ = (mod(position-phases(5),wavelengths(5)) <= imprecision);
 A = fft(a);
 B = fft(b);
 C = fft(c);
 D = fft(d);
-E = fft(e);
+E = fft(e_);
 CRT = A+B+C+D+E;
 crt = ifft(CRT);
 plot(real(crt))
